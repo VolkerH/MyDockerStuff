0b2343606f03b5c46a22393eb97867c61dd441de
Volker Hilsenstein <volker.hilsenstein@monash.edu>
2019-02-14 11:16:28 +0100

    adding Dockerfile for my python libapr experiments

---
+++ my_libapr/installation_script.sh
@@ -0,0 +1,13 @@
+git clone https://github.com/cheesema/LibAPR.git
+cd LibAPR
+git submodule update --init --recursive
+cd ..
+wget https://cmake.org/files/v3.10/cmake-3.10.2-Linux-x86_64.tar.gz
+tar -xzvf cmake-3.10.2-Linux-x86_64.tar.gz
+cd LibAPR
+mkdir build
+cd build
+/srv/cmake-3.10.2-Linux-x86_64/bin/cmake -DAPR_BUILD_PYTHON_WRAPPERS=ON ..
+make
+
+
